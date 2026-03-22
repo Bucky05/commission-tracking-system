60ee3b93461f0d97c827e8f73481170def4a8697
Bucky05 <anijodha@gmail.com>
2026-03-22 16:02:29 +0530

    referral application creation, updation and fetching handled

---
+++ src/routes/referral.js
@@ -0,0 +1,58 @@
+const router = require("express").Router();
+const role = require('../middlewares/roleMiddleware')
+const {
+  createReferralApplication,
+  getApplicationsByBrandId,
+  updateApplicationStatus,
+  getReferralLink
+} = require("../services/referralService");
+
+router.post("/", role(["creator"]), async (req, res) => {
+  try {
+    const { productId } = req.body;
+    const creatorId = req.user.id;
+
+    await createReferralApplication(productId, creatorId);
+    res.json("Application submitted");
+  } catch (err) {
+    res.status(400).json({ error: err });
+  }
+});
+
+router.get("/brand", role(["brand"]), async (req, res) => {
+  const brandId = req.user.id;
+  try {
+    const applications = await getApplicationsByBrandId(brandId);
+    res.json(applications);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+router.patch("/:id", role(["brand"]), async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { status } = req.body; // approved / rejected
+    await updateApplicationStatus(id,status)
+    res.json({ message: "Updated" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+router.get('/referral-link/:productId', role(['creator']), async (req, res) => {
+  try {
+    const { productId } = req.params;
+    const creatorId = req.user.id;
+
+    const link = await getReferralLink(creatorId, productId);
+
+    res.json({ referral_link: link });
+
+  } catch (err) {
+    res.status(403).json({ error: err.message });
+  }
+});
+
+
+module.exports = router
